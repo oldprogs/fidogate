efb076d0d64765c5173fa03ca3006145ca6f7bfb
mj <mj>
1996-04-17 18:26:18 +0000

    See ChangeLog

--- src/include/version.h
+++ src/include/version.h
@@ -2,10 +2,11 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 4.0 1996/04/17 18:17:42 mj Exp $
+ * $Id: version.h,v 4.1 1996/04/17 18:26:18 mj Exp $
  *
  * Global version number
  *****************************************************************************/
 
-#define VERSION		"$Revision: 4.0 $"
+/*#define VERSION		"$Revision: 4.1 $"*/
+#define VERSION		"REVISION: 4.0 "
 #define STATE		"$State: Exp $"
