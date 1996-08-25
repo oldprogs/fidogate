11961d5586545651b647be1dcac14293c1a22879
mj <mj>
1996-08-25 13:09:42 +0000

    See ChangeLog

--- src/include/patchlevel.h
+++ src/include/patchlevel.h
@@ -2,7 +2,7 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: patchlevel.h,v 4.3 1996/06/16 14:22:39 mj Exp $
+ * $Id: patchlevel.h,v 4.4 1996/08/25 13:09:47 mj Exp $
  *
  * Global patch level
  *****************************************************************************/
@@ -12,7 +12,7 @@
 /*
  * If uncommented, put "ALPHAn" after version id
  */
-#define ALPHA		3
+#define ALPHA		4
 
 /*
  * If uncommented, put "BETAn" after version id
