1fafa58c29992300c2e9353dc72ae6dfd898f2a6
mj <mj>
1996-09-07 16:40:42 +0000

    See ChangeLog

--- src/common/version.c
+++ src/common/version.c
@@ -2,23 +2,37 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.c,v 4.0 1996/04/17 18:17:40 mj Exp $
+ * $Id: version.c,v 4.1 1996/09/07 16:40:46 mj Exp $
  *
  * FIDOGATE version number handling stuff
  *
  *****************************************************************************
- * This version hacked and maintained by:
- *    _____ _____
- *   |	   |___	 |   Martin Junius	     FIDO:    2:2452/110	  2:242/6.0
- *   | | | |   | |   Republikplatz 3	     DOMAIN:  mj@dfv.rwth-aachen.de
- *   |_|_|_|_____|   W-5100 Aachen, Germany
+ * Copyright (C) 1990-1996
+ *  _____ _____
+ * |     |___  |   Martin Junius             FIDO:      2:2452/110
+ * | | | |   | |   Republikplatz 3           Internet:  mj@fido.de
+ * |_|_|_|@home|   D-52072 Aachen, Germany
  *
+ * This file is part of FIDOGATE.
+ *
+ * FIDOGATE is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * FIDOGATE is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FIDOGATE; see the file COPYING.  If not, write to the Free
+ * Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *****************************************************************************/
 
 #include "fidogate.h"
 
 #include "version.h"
-#include "patchlevel.h"
 
 
 
--- version.h
+++ version.h
@@ -2,10 +2,23 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 4.1 1996/08/26 19:10:27 mj Exp $
+ * $Id: version.h,v 4.2 1996/09/07 16:40:45 mj Exp $
  *
- * Global version number
+ * Global version number, patch level
  *****************************************************************************/
 
 #define VERSION		"REVISION: 4.0 "
 #define STATE		"STATE: alpha "
+
+
+#define PATCHLEVEL	0
+
+/*
+ * If uncommented, put "ALPHAn" after version id
+ */
+#define ALPHA		5
+
+/*
+ * If uncommented, put "BETAn" after version id
+ */
+/*#define BETA		0*/
