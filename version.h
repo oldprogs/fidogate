da0a1994ccdac9aa97049acebbf0528ecdda7b85
mj <mj>
1996-09-20 15:50:57 +0000

    See ChangeLog

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 /*****************************************************************************
  * FIDOGATE --- Gateway software UNIX Mail/News <-> FIDO NetMail/EchoMail
  *
- * $Id: version.h,v 4.3 1996/09/15 11:58:19 mj Exp $
+ * $Id: version.h,v 4.4 1996/09/20 15:50:57 mj Exp $
  *
  * Global version number, patch level
  *****************************************************************************/
 
 #define VERSION		"REVISION: 4.0 "
-#define STATE		"STATE: alpha "
+#define STATE		"STATE: beta "
 
 
 #define PATCHLEVEL	0
@@ -16,9 +16,9 @@
 /*
  * If uncommented, put "ALPHAn" after version id
  */
-#define ALPHA		6
+/*#define ALPHA		0*/
 
 /*
  * If uncommented, put "BETAn" after version id
  */
-/*#define BETA		0*/
+#define BETA		1
